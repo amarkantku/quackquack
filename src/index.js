15a25bd93f675038ac2376ce1d871e501e299719
Amarkant Kumar <amar.du2013@gmail.com>
2025-06-17 20:33:38 +0530

    feat: isPrime, factorial and fibonacci function add

--- src/index.js
+++ src/index.js
@@ -10,3 +10,35 @@ export function division(a, b) {
   if (b > 0) return a / b
   return -1
 }
+
+export function factorial(n) {
+  if (n < 0) return -1
+  if (n === 0 || n === 1) return 1
+  let result = 1
+  for (let i = 2; i <= n; i++) {
+    result *= i
+  }
+  return result
+}
+
+
+export function isPrime(n) {
+  if (n <= 1) return false
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false
+  }
+  return true
+}
+
+export function fibonacci(n) {
+  if (n < 0) return -1
+  if (n === 0) return 0
+  if (n === 1) return 1
+  let a = 0, b = 1, c
+  for (let i = 2; i <= n; i++) {
+    c = a + b
+    a = b
+    b = c
+  }
+  return b
+}
