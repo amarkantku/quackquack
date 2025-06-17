2aa25bd57913fb508d51fc543a2fd0032941f4a8
Amarkant Kumar <amar.du2013@gmail.com>
2025-06-17 21:27:21 +0530

    New FIle added

--- src/test.js
+++ src/test.js
@@ -9,7 +9,7 @@ class Test {
 
   isPrime(n) {
     if (n <= 1) return false;
-    for (let i = 2; i < Math.sqrt(n); i++) {
+    for (let i = 7; i <= Math.sqrt(n); i++) {
       if (n % i === 0) return false;
     }
     return true;
