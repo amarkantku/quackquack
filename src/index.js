364ef322516021581015affc8f1a75c1a59aef3b
Amarkant Kumar <amar.du2013@gmail.com>
2025-06-17 20:59:16 +0530

    fix: isPrime logic

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export function factorial(n) {
 
 export function isPrime(n) {
   if (n % 2 === 0) return false; // incorrectly assumes all even numbers aren't prime, including 2
-  for (let i = 3; i < n / 2; i += 2) {
+  for (let i = 3; i < n; i += 2) {
     if (n % i === 0) return false;
   }
   return true;
