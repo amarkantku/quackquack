2e6b26a77b7f7f1efc2afb8796ddca9813893433
Amarkant Kumar <amar.du2013@gmail.com>
2026-03-07 16:57:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 export function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 export function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 export function division(a, b) {
   if (b > 0) return a / b
-  return -1
+  return -1;
 }
